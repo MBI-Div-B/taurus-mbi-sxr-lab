9a73a5a25bbccd69c5b48db1efdbcf61e5e015d5
Turb0Pasca1 <wernicke@mbi-berlin.de>
2026-03-02 15:46:05 +0100

    refactored Panels

--- statusscreen/core/panel.py
+++ statusscreen/core/panel.py
@@ -2,6 +2,9 @@
 
 from pathlib import Path
 from taurus.external.qt import Qt
+from taurus.qt.qtgui.display import TaurusLabel, TaurusLed
+from taurus_pyqtgraph import TaurusTrend
+
 from PyQt5 import QtSvg
 class HeaderWidget(Qt.QWidget):
     # constructor to run when creating an instance of HeaderWidget 
@@ -40,3 +43,173 @@ class HeaderWidget(Qt.QWidget):
 
     def update_time(self):
         self.clock.setText(Qt.QDateTime.currentDateTime().toString('dddd dd.MM.yyyy - hh:mm:ss'))
+
+from taurus.external.qt import Qt
+from taurus.qt.qtgui.display import TaurusLabel
+
+class EnvironmentPanel(Qt.QWidget):
+    def __init__(self, title='Environment', parent=None):
+        super().__init__(parent)
+
+        # Main grid
+        layout = Qt.QGridLayout(self)
+        layout.setAlignment(Qt.Qt.AlignTop)
+
+        # Header
+        header_label = Qt.QLabel(title)
+        header_label.setObjectName('header')
+        layout.addWidget(header_label, 0, 0)
+
+        # Sensors
+        sensors = [
+            ['Pumps', 'lab/environment/pumps', (1, 0)],
+            ['Beamlines', 'sxr/environment/beamlines', (1, 3)],
+            ['PXS', 'sxr/environment/pxs', (2, 3)],
+            ['TDL Frontend', 'laser/environment/frontend', (1, 6)],
+            ['TDL Amplifier', 'laser/environment/thindisk', (2, 6)],
+            ['Compressor', 'laser/environment/compressor', (1, 9)],
+            ['TiSa', 'laser/environment/tisa', (2, 9)],
+        ]
+
+        for s in sensors:
+            # Name label
+            l = Qt.QLabel(s[0])
+            l.setAlignment(Qt.Qt.AlignRight)
+            # Temperature TaurusLabel
+            t = TaurusLabel()
+            t.setModel(s[1] + '/temperature')
+            t.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
+            layout.addWidget(l, s[2][0], s[2][1])
+            layout.addWidget(t, s[2][0], s[2][1]+1)
+
+class VacuumPanel(Qt.QWidget):
+    def __init__(self, title='Vacuum', parent=None):
+        super().__init__(parent)
+
+        # Main grid
+        layout = Qt.QGridLayout(self)
+        layout.setAlignment(Qt.Qt.AlignTop)
+
+        # Header label
+        header_label = Qt.QLabel(title)
+        header_label.setObjectName('header')
+        layout.addWidget(header_label, 0, 0)
+
+        # Subheaders
+        layout.addWidget(Qt.QLabel('RSXS'), 0, 2)
+        layout.addWidget(Qt.QLabel('Spectroscopy'), 0, 4)
+
+        # Gauges
+        gauges = [
+            ['PXS', 'sxr/tpg261/pxs', (1, 0)],
+            ['RZP', 'rsxs/tpg261/rzp', (1, 2)],
+            ['Scattering', 'rsxs/tpg261/scattering', (2, 2)],
+            ['Optics', 'spec/tpg261/optic', (1, 4)],
+            ['CCD', 'spec/tpg261/ccd', (2, 4)],
+        ]
+
+        for g in gauges:
+            # Name label
+            l = Qt.QLabel(g[0])
+            l.setAlignment(Qt.Qt.AlignRight)
+            # TaurusLabel for pressure
+            w = TaurusLabel()
+            w.setModel(g[1] + '/pressure')
+            w.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
+            w.setFormat('{:3.1e}')
+            w.setMinimumWidth(200)
+            layout.addWidget(l, g[2][0], g[2][1])
+            layout.addWidget(w, g[2][0], g[2][1]+1)
+
+class LaserPanel(Qt.QWidget):
+    def __init__(self, title='Laser Interlock', parent=None):
+        super().__init__(parent)
+
+        # Main layout for this panel
+        layout = Qt.QGridLayout(self)  # <- set layout on self
+        layout.setAlignment(Qt.Qt.AlignTop)
+
+        # ----- Laser header -----
+        laserLabel = Qt.QLabel(title)
+        laserLabel.setObjectName('header')
+        layout.addWidget(laserLabel, 0, 0)
+
+        # Door interlock
+        ledDoor = TaurusLed()
+        ledDoor.setOnColor("red")
+        ledDoor.setOffColor("green")
+        ledDoor.model = 'lab/rpigpio/laserpi/door_tisa'
+        ledDoor.setAlignment(Qt.Qt.AlignLeft)
+        ledDoor.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum)
+        layout.addWidget(ledDoor, 0, 1, 1, 3)
+
+        # TDL Laser
+        tdlLabel = Qt.QLabel("Thin Disk Laser")
+        tdlLabel.setObjectName('header')
+        layout.addWidget(tdlLabel, 1, 0)
+
+        ledShutterTDL= TaurusLed()
+        ledShutterTDL.setOnColor("red")
+        ledShutterTDL.setOffColor("green")
+        ledShutterTDL.model = 'lab/rpigpio/laserpi/shutter_tdl'
+        layout.addWidget(ledShutterTDL, 1, 1)
+
+        # TiSa Laser 
+        tisaLabel = Qt.QLabel("TiSa Laser")
+        tisaLabel.setObjectName('header')
+        layout.addWidget(tisaLabel, 1, 2)
+
+        ledShutterTiSa = TaurusLed()
+        ledShutterTiSa.setOnColor("red")
+        ledShutterTiSa.setOffColor("green")
+        ledShutterTiSa.model = 'lab/rpigpio/laserpi/shutter_tisa'
+        layout.addWidget(ledShutterTiSa, 1, 3)
+
+        # SC10 Shutter 
+        SC10shutterLabel = Qt.QLabel('Shutter')
+        layout.addWidget(SC10shutterLabel, 2, 0)
+        ledSC10shutter = TaurusLed()
+        ledSC10shutter.setOnColor("red")
+        ledSC10shutter.setOffColor("green")
+        ledSC10shutter.model = 'thindisk/thorlabssc10/seed/open'
+        layout.addWidget(ledSC10shutter, 2, 1)
+
+        # Waveplate 
+        waveplateLabel = Qt.QLabel("Waveplate")
+        layout.addWidget(waveplateLabel, 3, 0)
+        waveplateInd = TaurusLabel()
+        waveplateInd.setModel('thindisk/agilisagp/power/position')
+        waveplateInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
+        waveplateInd.setFixedWidth(200)
+        waveplateInd.setFormat('{:3.1f}')
+        layout.addWidget(waveplateInd, 3, 1)
+
+        # TDL & TiSa Energy 
+        tdlEnergyLabel = Qt.QLabel("Energy")
+        layout.addWidget(tdlEnergyLabel, 4, 0)
+        tdlEnergyInd = TaurusLabel()
+        tdlEnergyInd.setModel('thindisk/coherentpem/energymax/value')
+        tdlEnergyInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
+        tdlEnergyInd.setFixedWidth(200)
+        layout.addWidget(tdlEnergyInd, 4, 1)
+
+        tisaEnergyLabel = Qt.QLabel("Power")
+        layout.addWidget(tisaEnergyLabel, 4, 2)
+        tisaEnergyInd = TaurusLabel()
+        tisaEnergyInd.setModel('thindisk/thorlabspm100/regen/power')
+        tisaEnergyInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
+        tisaEnergyInd.setFixedWidth(200)
+        layout.addWidget(tisaEnergyInd, 4, 3)
+
+        # Trends at the bottom
+        tdlTrend = TaurusTrend()
+        tdlTrend.setModel(['thindisk/coherentpem/energymax/value'])
+        tdlTrend.setBackground('#222222')
+        tdlTrend.setMaxDataBufferSize(1000)
+        layout.addWidget(tdlTrend, 5, 0, 1, 2)  # row 5, spans 2 columns
+
+        tisaTrend = TaurusTrend()
+        tisaTrend.setModel(['thindisk/thorlabspm100/regen/power'])
+        tisaTrend.setBackground('#222222ff')
+        tisaTrend.setMaxDataBufferSize(1000)
+        layout.addWidget(tisaTrend, 5, 2, 1, 2)  # row 5, spans 2 columns
--- statusscreen/core/window.py
+++ statusscreen/core/window.py
@@ -1,13 +1,13 @@
 # core/window.py
-from .panel import HeaderWidget
+from .panel import HeaderWidget, EnvironmentPanel, VacuumPanel, LaserPanel
 
 from taurus.external.qt import Qt
 # from taurus.qt.qtgui.application import TaurusApplication
-from taurus.qt.qtgui.display import TaurusLabel, TaurusLed
+# from taurus.qt.qtgui.display import TaurusLabel, TaurusLed
 from pathlib import Path
-from PyQt5 import QtSvg
+# from PyQt5 import QtSvg
 
-from taurus_pyqtgraph import TaurusTrend
+# from taurus_pyqtgraph import TaurusTrend
 
 
 class BaseWindow(Qt.QMainWindow):
@@ -27,6 +27,7 @@ class BaseWindow(Qt.QMainWindow):
         style_path = base_dir / 'styles' / 'mbi-styles.qss'
         self.setStyleSheet(style_path.read_text())
 
+        # switch to different icon...
         self.setWindowIcon(Qt.QIcon(str(base_dir / 'styles' / 'mbi-logo.png')))
 
         # set base layout to align the panels
@@ -38,197 +39,20 @@ class BaseWindow(Qt.QMainWindow):
         self.header = HeaderWidget()
         self.central_layout.addWidget(self.header)
 
-        ### environment vacuum
+        env_panel = EnvironmentPanel()
+        vac_panel = VacuumPanel()
 
-        # environment
-        envWidget = Qt.QWidget(self)
-        envLayout = Qt.QGridLayout(envWidget)
-        envLayout.setAlignment(Qt.Qt.AlignTop)
+        envvac_widget = Qt.QWidget()
+        envvac_layout = Qt.QHBoxLayout(envvac_widget)
+        envvac_layout.addWidget(env_panel)
+        envvac_layout.addItem(Qt.QSpacerItem(20, 40, Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum))
+        envvac_layout.addWidget(vac_panel)
+        envvac_layout.setAlignment(Qt.Qt.AlignTop)
 
-        envLabel = Qt.QLabel('Environment')
-        envLabel.setObjectName('header')
-        envLayout.addWidget(envLabel, 0, 0)
+        self.central_layout.addWidget(envvac_widget)
 
-        sensors = [
-            ['Pumps', 'lab/environment/pumps', (1, 0)],
-            ['Beamlines', 'sxr/environment/beamlines', (1, 3)],
-            ['PXS', 'sxr/environment/pxs', (2, 3)],
-            ['TDL Frontend', 'laser/environment/frontend', (1, 6)],
-            ['TDL Amplifier', 'laser/environment/thindisk', (2, 6)],
-            ['Compressor', 'laser/environment/compressor', (1, 9)],
-            ['TiSa', 'laser/environment/tisa', (2, 9)],
-                  ]  
-        
-        for s in sensors:
-            t = TaurusLabel()            
-            t.setModel(s[1] + '/temperature')
-            t.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-            # h = TaurusLabel()            
-            # h.setModel(s[1] + '/humidity')
-            # h.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-
-            l = Qt.QLabel(s[0])
-            l.setAlignment(Qt.Qt.AlignRight)
-            envLayout.addWidget(l, s[2][0], s[2][1])
-            envLayout.addWidget(t, s[2][0], s[2][1]+1)
-            # envLayout.addWidget(h, s[2][0], s[2][1]+2)
-
-
-        # vacuum
-        vacWidget = Qt.QWidget(self)
-        vacLayout = Qt.QGridLayout(vacWidget)
-        vacLayout.setAlignment(Qt.Qt.AlignTop)
-
-        vacLabel = Qt.QLabel('Vacuum')
-        vacLabel.setObjectName('header')
-        vacLayout.addWidget(vacLabel, 0, 0)
-
-        vacLabel = Qt.QLabel('RSXS')
-        #vacLabel.setObjectName('header')
-        vacLayout.addWidget(vacLabel, 0, 2)
-
-        vacLabel = Qt.QLabel('Spectroscopy')
-        #vacLabel.setObjectName('header')
-        vacLayout.addWidget(vacLabel, 0, 4)
-
-        gauges = [
-            ['PXS', 'sxr/tpg261/pxs', (1, 0)],
-            ['RZP', 'rsxs/tpg261/rzp', (1, 2)],
-            ['Scattering', 'rsxs/tpg261/scattering', (2, 2)],
-            ['Optics', 'spec/tpg261/optic', (1, 4)],
-            ['CCD', 'spec/tpg261/ccd', (2, 4)],
-                  ]  
-        
-        for g in gauges:
-            w = TaurusLabel()            
-            w.setModel(g[1] + '/pressure')
-            w.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-            w.setFormat('{:3.1e}')
-            w.setMinimumWidth(200)
-
-            l = Qt.QLabel(g[0])
-            l.setAlignment(Qt.Qt.AlignRight)
-            vacLayout.addWidget(l, g[2][0], g[2][1])
-            vacLayout.addWidget(w, g[2][0], g[2][1]+1)
-
-        #
-
-        envvacWidget = Qt.QWidget(self)
-        envvacLayout = Qt.QHBoxLayout(envvacWidget)
-        envvacLayout.addWidget(envWidget)
-        envvacLayout.addItem(Qt.QSpacerItem(20, 40, Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum))
-        envvacLayout.addWidget(vacWidget)
-        envvacLayout.setAlignment(Qt.Qt.AlignTop)
-
-        ### laser
-
-        laserLabel = Qt.QLabel('Laser Interlock')
-        laserLabel.setObjectName('header')
-        # Door Interlock
-        ledDoor = TaurusLed()
-        ledDoor.setOnColor("red")
-        ledDoor.setOffColor("green")
-        ledDoor.model = 'lab/rpigpio/laserpi/door_tisa'
-        ledDoor.setAlignment(Qt.Qt.AlignLeft)
-        ledDoor.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum)
-
-        # TDL
-        tdlLabel = Qt.QLabel("Thin Disk Laser")
-        tdlLabel.setObjectName('header')
-
-        ledShutterTDL= TaurusLed()
-        ledShutterTDL.setOnColor("red")
-        ledShutterTDL.setOffColor("green")
-        ledShutterTDL.model = 'lab/rpigpio/laserpi/shutter_tdl'
-        ledShutterTDL.setAlignment(Qt.Qt.AlignLeft)
-        ledShutterTDL.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum)
-
-        SC10shutterLabel = Qt.QLabel('Shutter')
-        #SC10shutterLabel.setAlignment(Qt.Qt.AlignRight)
-        ledSC10shutter = TaurusLed()
-        ledSC10shutter.setOnColor("red")
-        ledSC10shutter.setOffColor("green")
-        ledSC10shutter.model = 'thindisk/thorlabssc10/seed/open'
-        ledSC10shutter.setAlignment(Qt.Qt.AlignLeft)
-
-        waveplateLabel = Qt.QLabel("Waveplate")
-        #waveplateLabel.setAlignment(Qt.Qt.AlignRight)
-        waveplateInd = TaurusLabel()
-        waveplateInd.setModel('thindisk/agilisagp/power/position')
-        waveplateInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-        waveplateInd.setFixedWidth(200)
-        waveplateInd.setFormat('{:3.1f}')
-
-        tdlEnergyLabel = Qt.QLabel("Energy")
-        #tdlEnergyLabel.setAlignment(Qt.Qt.AlignRight)
-        tdlEnergyInd = TaurusLabel()            
-        tdlEnergyInd.setModel('thindisk/coherentpem/energymax/value')
-        tdlEnergyInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-        tdlEnergyInd.setFixedWidth(200)
-
-        tdlTrend = TaurusTrend()
-        tdlTrend.setModel(['thindisk/coherentpem/energymax/value'])
-        tdlTrend.setBackground(background='#222222')
-        tdlTrend.setMaxDataBufferSize(1000)
-        #tdlTrend.setForcedReadingPeriod(500)
-
-        # TiSa
-        tisaLabel = Qt.QLabel("TiSa Laser")
-        tisaLabel.setObjectName('header')
-
-        ledShutterTiSa = TaurusLed()
-        ledShutterTiSa.setOnColor("red")
-        ledShutterTiSa.setOffColor("green")
-        ledShutterTiSa.model = 'lab/rpigpio/laserpi/shutter_tisa'
-        ledShutterTiSa.setAlignment(Qt.Qt.AlignLeft)
-        ledShutterTiSa.setSizePolicy(Qt.QSizePolicy.Expanding, Qt.QSizePolicy.Minimum)
-
-        
-        tisaEnergyLabel = Qt.QLabel("Power")
-        #tisaEnergyLabel.setAlignment(Qt.Qt.AlignRight)
-        tisaEnergyInd = TaurusLabel()            
-        tisaEnergyInd.setModel('thindisk/thorlabspm100/regen/power')
-        tisaEnergyInd.setAlignment(Qt.Qt.AlignRight | Qt.Qt.AlignVCenter)
-        tisaEnergyInd.setFixedWidth(200)
-
-        tisaTrend = TaurusTrend()
-        tisaTrend.setModel(['thindisk/thorlabspm100/regen/power'])
-        tisaTrend.setBackground(background='#222222ff')
-        tisaTrend.setMaxDataBufferSize(1000)
-        #tisaTrend.setForcedReadingPeriod(500)
-
-        #  
-
-        laserWidget = Qt.QWidget(self)
-        laserLayout = Qt.QGridLayout(laserWidget)
-
-        laserLayout.addWidget(laserLabel, 0, 0)
-        laserLayout.addWidget(ledDoor, 0, 1, 1, 3)
-
-        laserLayout.addWidget(tdlLabel, 1, 0)
-        laserLayout.addWidget(ledShutterTDL, 1, 1)    
-        laserLayout.addWidget(tisaLabel, 1, 2)
-        laserLayout.addWidget(ledShutterTiSa, 1, 3)   
-    
-        laserLayout.addWidget(SC10shutterLabel, 2, 0)
-        laserLayout.addWidget(ledSC10shutter, 2, 1)   
-        
-        laserLayout.addWidget(waveplateLabel, 3, 0)
-        laserLayout.addWidget(waveplateInd, 3, 1)   
-
-        laserLayout.addWidget(tdlEnergyLabel, 4, 0)
-        laserLayout.addWidget(tdlEnergyInd, 4, 1)    
-        laserLayout.addWidget(tisaEnergyLabel, 4, 2)
-        laserLayout.addWidget(tisaEnergyInd, 4, 3)
-        laserLayout.addWidget(tdlTrend, 5, 0, 1, 2)
-        laserLayout.addWidget(tisaTrend, 5, 2, 1, 2)
-
-        ### central
-        #centralWidget = Qt.QWidget(self)
-        #centralLayout = Qt.QVBoxLayout(centralWidget)
-        #self.central_layout.addWidget(self.header)
-        self.central_layout.addWidget(envvacWidget)
-        self.central_layout.addWidget(laserWidget)
+        self.laser = LaserPanel()
+        self.central_layout.addWidget(self.laser)
 
         self.central_layout.setAlignment(Qt.Qt.AlignTop)
 
@@ -236,7 +60,3 @@ class BaseWindow(Qt.QMainWindow):
         # Set the central widget of the Window.
         # self.setCentralWidget(self.centralWidget)
         
-    def showTime(self):
-        current_time=Qt.QDateTime.currentDateTime()
-        formatted_time=current_time.toString('dddd dd.MM.yyyy - hh:mm:ss')
-        self.clock.setText(formatted_time)
